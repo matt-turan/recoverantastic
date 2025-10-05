675d6770ac4b1869891fbb95d3f73ed461fe6624
Merzifonlu Kara Mustafa Pasa <scarlet.wings.creative@gmail.com>
2025-10-05 15:17:05 +0000

    chore: Add no-index to private app pages

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,14 @@
 'use client';
 
 import { AppShell } from '@/components/layout/app-shell';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
 
 export default function AppPagesLayout({
   children,
