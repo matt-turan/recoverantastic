74421874d8dc9d903fb460452bc491694e9669ed
Merzifonlu Kara Mustafa Pasa <scarlet.wings.creative@gmail.com>
2025-10-11 13:06:51 +0000

    The app isn't starting. Please investigate what could be wrong based on

---
+++ Users/johndoe/recoverantastic/next.config.ts
@@ -0,0 +1,32 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      }
+    ],
+  },
+  serverExternalPackages: ['firebase-admin'],
+};
+// export
+export default nextConfig;
